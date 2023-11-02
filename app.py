cf2c70f02b5d89db2305985c18f99eccab155e0a
matthewmintz08 <69319245+matthewmintz08@users.noreply.github.com>
2023-11-02 19:15:10 +0000

    Added 3d point cloud

--- app.py
+++ app.py
@@ -2,22 +2,44 @@ from flask import *
 from fileinput import filename
 from distutils.log import debug
 import numpy as np
-import pyvista as pv
 import ezdxf as ez
+import os
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {'dxf'}
 
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'tmp'
+
+
+def allowed_file(filename):
+  return '.' in filename and \
+         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def get_points_from_start_to_end(e, standard_point_scale):
   st = np.array([e.dxf.start.x, e.dxf.start.y, 0])
   ed = np.array([e.dxf.end.x, e.dxf.end.y, 0])
+  st2 = np.array([e.dxf.start.x, e.dxf.start.y, 393.7])
+  ed2 = np.array([e.dxf.end.x, e.dxf.end.y, 393.7])
+  st3 = np.array([e.dxf.start.x, e.dxf.start.y, 236.22])
+  ed3 = np.array([e.dxf.end.x, e.dxf.end.y, 236.22])
 
   dist_btw = np.linalg.norm(ed - st)
+  dist_btw2 = np.linalg.norm(ed2 - st2)
+  dist_btw3 = np.linalg.norm(ed3 - st3)
   num_points = int(dist_btw / standard_point_scale)
+  num_points2 = int(dist_btw2 / standard_point_scale)
+  num_points3 = int(dist_btw3 / standard_point_scale)
 
   # Generate intermediate points
   points = [st + i * (ed - st) / num_points for i in range(num_points)]
+  points += [st2 + i * (ed2 - st2) / num_points2 for i in range(num_points2)]
+  points += [st3 + i * (ed3 - st3) / num_points3 for i in range(num_points3)]
   points.append(ed)  # Add end point
+  points.append(ed2)
+  points.append(ed3)
   return points
 
 
@@ -27,7 +49,7 @@ def get_midpoint(e):
   return (st + ed) / 2
 
 
-def generate_point_cloud(dxfFile):
+def generate_point_cloud(dxfFile, name):
   try:
     doc = ez.readfile(dxfFile)
   except IOError:
@@ -61,28 +83,45 @@ def generate_point_cloud(dxfFile):
 
   pointCloud = np.asarray(cloud_point_array)
   pointCloudMeters = (pointCloud * 0.0254)
-  print(pointCloudMeters)
-  return (pointCloudMeters)
-  #np.savetxt("point_cloud.xyz", pointCloud)
-  np.savetxt("point_cloud_meters.xyz", pointCloudMeters)
+  pointCloudName = name + ".xyz"
+  np.savetxt("tmp/" + pointCloudName, pointCloudMeters)
+  return pointCloudName
 
 
-@app.route("/")
-def main():
+@app.route("/", methods=['GET', 'POST'])
+def upload_file():
+  if request.method == 'POST':
+    file = request.files["file"]
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return redirect(url_for('success', name=filename))
   return render_template("index.html")
 
 
-@app.route("/success", methods=['POST'])
-def success():
-  if request.method == 'POST':
-    f = request.files['file']
-    f.save(f.filename)
-    generate_point_cloud(f.filename)
-    return render_template("Acknowledgement.html", name=f.filename)
+@app.route("/success/<name>")
+def success(name):
+  file = os.path.join(app.config['UPLOAD_FOLDER'], name)
+  base, ext = os.path.splitext(name)
+  pointCloudName = generate_point_cloud(file, base)
+  return render_template("Acknowledgement.html",
+                         name=name,
+                         pointCloudName=pointCloudName)
 
-@app.route("/download", methods=['POST'])
-def download():
-  return render_template("Download.html")
+
+@app.route("/download/<pointCloudName>")
+def download(pointCloudName):
+
+  @after_this_request
+  def clear_files(response):
+    base, ext = os.path.splitext(pointCloudName)
+    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], base + ".xyz"))
+    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], base + ".dxf"))
+    return response
+    
+  print("hello")
+  print(pointCloudName)
+  return send_from_directory(app.config['UPLOAD_FOLDER'], pointCloudName)
 
 
 if __name__ == "__main__":
